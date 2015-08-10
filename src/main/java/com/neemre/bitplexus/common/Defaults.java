6db08362613be12167c35da18e6ccbcade1dc2e0
Priidu Neemre <priidu.neemre@gmail.com>
2015-08-10 22:16:34 +0300

    Fix RPC API/notification API ports in Bitcoin Core & Litecoin Core configuration files 'bitcoin.conf' & 'litecoin.conf' (set to default values); modify 'testnet3_config.properties' & 'services-config.xml'

--- src/main/java/com/neemre/bitplexus/common/Defaults.java
+++ src/main/java/com/neemre/bitplexus/common/Defaults.java
@@ -6,8 +6,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Defaults {
 
+	public static final int UNCONFIRMED_CONF_COUNT = 0;
 	public static final int CONFIRMED_CONF_COUNT = 1;
-	public static final int COMPLETED_CONF_COUNT = 3;
+	public static final int BTC_COMPLETED_CONF_COUNT = 3;
+	public static final int LTC_COMPLETED_CONF_COUNT = 12;
 	
 	public static final int PASSPHRASE_TIMEOUT = 5;
 }
