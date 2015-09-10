7f09f3ec5f57343be0b9c16a6464ed63d251136c
Priidu Neemre <priidu.neemre@gmail.com>
2015-09-10 20:57:58 +0300

    (Amend previous commit)

---
+++ src/main/java/com/neemre/bitplexus/backend/crypto/LockedTransactionPool.java
@@ -0,0 +1,43 @@
+package com.neemre.bitplexus.backend.crypto;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+import javax.annotation.Resource;
+
+import org.springframework.scheduling.TaskScheduler;
+
+import com.neemre.bitplexus.common.util.DateUtils;
+
+public class LockedTransactionPool {
+
+	private static final int TXN_RELEASE_DELAY = 5;
+
+	private Set<String> networkUids;
+
+	@Resource(name = "commonScheduler")
+	private TaskScheduler scheduler;
+
+
+	public LockedTransactionPool() {
+		networkUids = new ConcurrentSkipListSet<String>();
+	}
+
+	public boolean addTransaction(String networkUid) {
+		return networkUids.add(networkUid);
+	}
+
+	public boolean containsTransaction(String networkUid) {
+		return networkUids.contains(networkUid);
+	}
+
+	public void removeTransaction(final String networkUid) {
+		scheduler.schedule(new Runnable() {
+			@Override
+			public void run() {
+				networkUids.remove(networkUid);
+			}
+		}, DateUtils.addSeconds(new Date(), TXN_RELEASE_DELAY));
+	}
+}
